64ea85ca7b6a03fcaa6c4fa009e8512d4096de2f
Kenny Ohata <59850891+kennyohata@users.noreply.github.com>
2020-07-15 07:30:39 -0700

    Add files via upload

---
+++ house_prices.py
@@ -0,0 +1,15 @@
+
+# Import all the libraries 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from scipy import stats
+from scipy.stats import norm, skew
+
+import warnings
+warnings.filterwarnings('ignore')
+#%matplotlib inline
+
+
